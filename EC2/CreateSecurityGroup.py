aed98b2e89755c37becc8d6a3e3afc1b8993f320
drwhitfield <43507455+drwhitfield@users.noreply.github.com>
2021-04-04 13:21:12 +0200

    Update CreateSecurityGroup.py

--- EC2/CreateSecurityGroup.py
+++ EC2/CreateSecurityGroup.py
@@ -20,27 +20,32 @@ ec2 = boto3.client('ec2')
 response = ec2.describe_vpcs()
 vpc_id = response.get('Vpcs', [{}])[0].get('VpcId', '')
 
-try:
-    response = ec2.create_security_group(GroupName='HSB_SECURITY_GROUP',
+
+def CreateSecurityGroup():
+    
+    try:
+        response = ec2.create_security_group(GroupName='HSB_SECURITY_GROUP',
                                          Description='Host Services Block',
                                          VpcId=vpc_id)
-    security_group_id = response['GroupId']
-    print('Security Group Created %s in vpc %s.' % (security_group_id, vpc_id))
+        security_group_id = response['GroupId']
+        print('Security Group Created %s in vpc %s.' % (security_group_id, vpc_id))
 
-    data = ec2.authorize_security_group_ingress(
-        GroupId=security_group_id,
-        IpPermissions=[
-            {'IpProtocol': 'tcp',
-             'FromPort': 80,
-             'ToPort': 80,
-             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
-            {'IpProtocol': 'tcp',
-             'FromPort': 443,
-             'ToPort': 443,
-             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
-        ])
+        data = ec2.authorize_security_group_ingress(
+            GroupId=security_group_id,
+            IpPermissions=[
+                {'IpProtocol': 'tcp',
+                 'FromPort': 80,
+                 'ToPort': 80,
+                 'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+                {'IpProtocol': 'tcp',
+                 'FromPort': 443,
+                 'ToPort': 443,
+                 'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
+            ])
     
-    print('Ingress Successfully Set %s' % data)
-except ClientError as e:
-    print(e)
+        print('Ingress Successfully Set %s' % data)
+    except ClientError as e:
+        print(e)
     
+if __name__ == '__main__':
+    CreateSecurityGroup()
