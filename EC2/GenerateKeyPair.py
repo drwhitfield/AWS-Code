97351219d512b51411996d793d983bc654266db0
drwhitfield <43507455+drwhitfield@users.noreply.github.com>
2021-04-04 14:00:00 +0200

    Create GenerateKeyPair.py

---
+++ EC2/GenerateKeyPair.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+"""
+
+Script to Generate New AWS SSH Keys
+
+"""
+
+__author__    = ' Donald R. Whitfield'
+__copyright__ = 'S-Box Security, LLC'
+__email__     = 'donaldwhitfield@icloud.com'
+__status__    = 'Development'
+
+
+import boto3
+import os
+
+def Create_Key_Pair():
+    ec2_client = boto3.client("ec2", region_name="eu-central-1")
+    key_pair = ec2_client.create_key_pair(KeyName="ec2-key-pair")
+
+    private_key = key_pair["KeyMaterial"]
+
+    # write private key to file with 400 permissions
+    with os.fdopen(os.open("/tmp/aws_ec2_key.pem", os.O_WRONLY | os.O_CREAT, 0o400), "w+") as handle:
+        handle.write(private_key)
+
+Create_Key_Pair() 
