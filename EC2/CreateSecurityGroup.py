982456b92874e55958513340a2ffb85d1cc72514
drwhitfield <43507455+drwhitfield@users.noreply.github.com>
2021-04-04 13:11:30 +0200

    Create CreateSecurityGroup.py

---
+++ EC2/CreateSecurityGroup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+"""
+
+Simple Script to Create New AWS EC2 Security Group
+Specifically for Externally Facing Services Over Ports 80,443
+
+"""
+
+__author__    = ' Donald R. Whitfield'
+__copyright__ = 'S-Box Security, LLC'
+__email__     = 'donaldwhitfield@icloud.com'
+__status__    = 'Development'
+
+import boto3
+from botocore.exceptions import ClientError
+
+ec2 = boto3.client('ec2')
+
+response = ec2.describe_vpcs()
+vpc_id = response.get('Vpcs', [{}])[0].get('VpcId', '')
+
+try:
+    response = ec2.create_security_group(GroupName='HSB_SECURITY_GROUP',
+                                         Description='Host Services Block',
+                                         VpcId=vpc_id)
+    security_group_id = response['GroupId']
+    print('Security Group Created %s in vpc %s.' % (security_group_id, vpc_id))
+
+    data = ec2.authorize_security_group_ingress(
+        GroupId=security_group_id,
+        IpPermissions=[
+            {'IpProtocol': 'tcp',
+             'FromPort': 80,
+             'ToPort': 80,
+             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+            {'IpProtocol': 'tcp',
+             'FromPort': 443,
+             'ToPort': 443,
+             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
+        ])
+    
+    print('Ingress Successfully Set %s' % data)
+except ClientError as e:
+    print(e)
+    
